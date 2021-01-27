6698b220d250a83614f8d3e7351d8fa060a93f61
leightonvanrooijen <leightonvanrooijen@gmail.com>
2021-01-28 11:08:59 +1300

    chnaged for production api

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@ap
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000"
+  uri: "https://arcane-hamlet-31547.herokuapp.com/"
 })
 
 const authLink = setContext(() => {
